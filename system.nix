fa46079bff1cf34d449ae9f8827794e199682e67
Jake Stanger <mail@jstanger.dev>
2026-05-01 22:43:06 +0100

    feat(system): use garnix cache

--- system.nix
+++ system.nix
@@ -10,9 +10,15 @@
   i18n.defaultLocale = "en_GB.UTF-8";
   console.keyMap = "uk";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
-  nix.gc.automatic = false;
-  nix.optimise.automatic = true;
+  nix = {
+    settings.experimental-features =
+      [ "nix-command" "flakes" "pipe-operators" ];
+    gc.automatic = false;
+    optimise.automatic = true;
+
+    settings.substituters = ["https://cache.garnix.io"];
+    settings.trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.flake = "github:jakestanger/nixos-config#${hostInfo.name}";
@@ -27,7 +33,8 @@
 
   sops = {
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    defaultSopsFile = ./secrets/${lib.fixedWidthString 2 "0" (toString hostInfo.num)}.yaml;
+    defaultSopsFile =
+      ./secrets/${lib.fixedWidthString 2 "0" (toString hostInfo.num)}.yaml;
   };
 
   system.stateVersion = "25.11"; # INIT VERSION - DO NOT TOUCH!
