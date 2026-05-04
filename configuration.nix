f23a2a10468d87476970b6b657b0bb2474f9eade
Jake Stanger <mail@jstanger.dev>
2026-05-04 13:51:52 +0100

    feat: add 02 secret

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ ... }:
+{ lib, hostInfo, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -11,6 +11,6 @@
     ./monitoring.nix
     ./containers.nix
     ./shell.nix
-  ];
+  ]  ++ lib.optional (builtins.pathExists ./machines/${hostInfo.name}.nix) ./machines/${hostInfo.name}.nix;
 }
 
---
+++ machines/penelope-02.nix
@@ -0,0 +1,4 @@
+{ ... }:
+{
+  sops.secrets."postgres/env" = {};
+}
